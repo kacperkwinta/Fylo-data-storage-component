41197bcce3eea5099410122084b39a5ac6c160be
turbobaguettes <kaspersky.kwinta@gmail.com>
2023-03-17 22:44:15 +0100

    mobile styles

--- js/script.js
+++ js/script.js
@@ -3,8 +3,12 @@
 const usedGB = document.getElementById("usedGB");
 const emptyGB = document.getElementById("emptyGB");
 const input = document.querySelector(".range");
+const inputFill = document.querySelector(".fill");
 
+// setting amount of GB
 input.addEventListener("input", function () {
 	usedGB.textContent = input.value;
 	emptyGB.textContent = 1000 - Number(input.value);
+	//  filling bar
+	inputFill.style.width = `${input.value * 0.1}%`;
 });
